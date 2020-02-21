5617d398c0719b68ce16e9221e2a5ba97a07ce17
Táskai Dominik <taskaidomi@gmail.com>
2020-02-21 22:57:38 +0100

    TwoSum LC

---
+++ src/problems/TwoSum/TwoSumImplementation.java
@@ -0,0 +1,16 @@
+package problems.TwoSum;
+
+public class TwoSumImplementation {
+    public static int[] twoSum(int[] nums, int target) {
+        int subtarget;
+        for(int i=0;i<nums.length;i++){
+            subtarget=target-nums[i];
+            for(int j=0;i<nums.length;j++){
+                if(nums[j]==subtarget){
+                    return new int []{i,j};
+                }
+            }
+        }
+        return new int []{-1,-1};
+    }
+}
